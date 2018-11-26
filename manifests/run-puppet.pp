21d1c7aefc8482662d89ab91c73f4eb8124e3129
Gary <bigminer@gmail.com>
2018-11-26 13:49:48 -0600

    added hello.txt ensure with content

--- manifests/run-puppet.pp
+++ manifests/run-puppet.pp
@@ -4,6 +4,11 @@ file { '/usr/local/bin/run-puppet':
   mode   => '0755',
 }
 
+file { '/tmp/hello.txt':
+  ensure => file,
+  conent   => 'hello, world\n',
+}
+
 cron { 'run-puppet':
   command => '/usr/local/bin/run-puppet',
   hour    => '*',
